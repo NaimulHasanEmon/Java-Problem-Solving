c21915570251e367d5c5009215e516ada6be2b6d
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-01-29 02:15:45 +0600

    Update Scan2DArrayAndPrint.java

--- Scan2DArrayAndPrint.java
+++ Scan2DArrayAndPrint.java
@@ -4,8 +4,7 @@ public class Scan2DArrayAndPrint {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("How many rows and columns you want?");
-        int row = sc.nextInt();
-        int col = sc.nextInt();
+        int row = sc.nextInt(), col = sc.nextInt();
         int arr[][] = new int[row][col];
         System.out.println("Enter the array numbers: ");
         for (int i = 0; i < row; i++) {
