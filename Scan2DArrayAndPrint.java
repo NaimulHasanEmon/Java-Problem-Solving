b2e4df32e106957587f6bfe627de87d50f17db51
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-01-29 02:20:51 +0600

    Update Scan2DArrayAndPrint.java

--- Scan2DArrayAndPrint.java
+++ Scan2DArrayAndPrint.java
@@ -1,17 +1,18 @@
 import java.util.Scanner;
 
-public class Scan2DArrayAndPrint {
+public class SumOf2DArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("How many rows and columns you want?");
         int row = sc.nextInt(), col = sc.nextInt();
         int arr[][] = new int[row][col];
-        System.out.println("Enter the array numbers: ");
+        System.out.println("Enter array elements: ");
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
                 arr[i][j] = sc.nextInt();
             }
         }
+        System.out.println("Your array elements are: ");
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
                 System.out.print(arr[i][j] + " ");
