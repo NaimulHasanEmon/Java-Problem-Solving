afe447b6c841dd5e81705f17e2b0bfe2fe0a763e
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-01-29 12:23:11 +0600

    Add files via upload

---
+++ ScanAndSearchIn2DArray.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ScanAndSearchIn2DArray {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("How many rows and columns you want?");
+		int row = sc.nextInt(), col = sc.nextInt();
+		int arr[][] = new int[row][col];
+		System.out.println("Enter array elements: ");
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < col; j++) {
+				arr[i][j] = sc.nextInt();
+			}
+		}
+		System.out.print("Enter the number you want to search: ");
+		int n = sc.nextInt();
+		int count = 1;
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < col; j++) {
+				if (arr[i][j] == n) {
+					System.out.println(n + " found at [" + i + "," + j + "] index.");
+					count = 0;
+				}
+			}
+		}
+		if (count > 0)
+			System.out.println("Sorry! " + n + " is absent in the database.");
+		sc.close();
+	}
+}
