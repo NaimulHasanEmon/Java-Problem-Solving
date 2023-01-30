33b01443c34ec4e2c58eadccc71d9a19a52e80b6
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-01-31 02:43:08 +0600

    Add files via upload

---
+++ SetBitOperation.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class SetBitOperation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        System.out.print("Enter position: ");
+        int pos = sc.nextInt();
+        int bitMask = 1 << pos;
+        System.out.println("New Number: " + (bitMask | num));
+        sc.close();
+    }
+}
