7733755dc1b3af7c02f47a32c196b1737bd2ac9f
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-01-31 01:45:25 +0600

    Update ReverseString.java

--- ReverseString.java
+++ ReverseString.java
@@ -4,8 +4,7 @@ public class ReverseString {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a string: ");
-        String name = sc.nextLine();
-        StringBuilder sb = new StringBuilder(name);
+        StringBuilder sb = new StringBuilder(sc.nextLine());
         System.out.print("Reversed string is: ");
         for (int i = sb.length() - 1; i >= 0; i--) {
             System.out.print(sb.charAt(i));
