2f2a2ffea5d647a2bb186a2b00f38bbf71699899
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-01-31 02:34:29 +0600

    Update and rename getBitOperation.java to GetBitOperation.java

--- GetBitOperation.java
+++ GetBitOperation.java
@@ -1,9 +1,9 @@
 import java.util.*;
 
-public class getBitOperation {
+public class GetBitOperation {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a binary number: ");
+        System.out.print("Enter a number: ");
         int num = sc.nextInt();
         System.out.print("Enter position: ");
         int pos = sc.nextInt();
