6218d24a56cc5b98c463e956e0084bc03dda4d2b
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-01-31 20:14:55 +0600

    Add files via upload

---
+++ ArrayWithoutScanning.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ArrayWithoutScanning {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter row: ");
+        int row = sc.nextInt();
+        System.out.print("Enter column: ");
+        int col = sc.nextInt();
+        int i, j, n = 1, m = col;
+        for (i = 1; i <= row; i++) {
+            for (j = n; j <= m; j++) {
+                System.out.print(j + "\t");
+            }
+            n = j;
+            m = j + col - 1;
+            System.out.println();
+        }
+        sc.close();
+    }
+}
