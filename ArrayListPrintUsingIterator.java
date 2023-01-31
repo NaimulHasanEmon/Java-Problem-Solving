578bc8898091d6098370e5e8d6dc907e1d357467
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-02-01 00:30:12 +0600

    Add files via upload

---
+++ ArrayListPrintUsingIterator.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class ArrayListPrintUsingIterator {
+    public static void main(String[] args) {
+        ArrayList<Integer> num = new ArrayList<>();
+        num.add(10);
+        num.add(11);
+        num.add(12);
+        num.add(3, 20);
+        Iterator itr = num.iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next() + " ");
+        }
+    }
+}
