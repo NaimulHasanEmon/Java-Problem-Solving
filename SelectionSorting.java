dac80284908cad3714de7f557f8f9c923be019f9
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-01-31 16:30:52 +0600

    Add files via upload

---
+++ SelectionSorting.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class SelectionSorting {
+    public static void printArray(int arr[]) {
+        System.out.print("Sorted array is: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("How many numbers you want to check: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Time complexity = O(n^2)
+        // Selection sort
+        for (int i = 0; i < arr.length - 1; i++) {
+            int smallest = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[smallest] > arr[j]) {
+                    smallest = j;
+                }
+            }
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+        }
+        printArray(arr);
+        sc.close();
+    }
+}
