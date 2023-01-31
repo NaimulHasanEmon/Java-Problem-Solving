5c6ef3950fa3f11281f9a9130a623830543cf32d
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-01-31 13:23:17 +0600

    Add files via upload

---
+++ BubbleSorting2.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class BubbleSorting2 {
+    public static void printArray(int arr[]) {
+        System.out.print("Sorted array is: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("How many numbers you want to check: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        // bubble sort
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        printArray(arr);
+        sc.close();
+    }
+}
