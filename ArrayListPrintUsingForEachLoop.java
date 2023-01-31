7c6655879851327e83207d35e1abd8d51b80124a
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-02-01 00:02:41 +0600

    Add files via upload

---
+++ ArrayListPrintUsingForEachLoop.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class ArrayListPrintUsingForEachLoop {
+    public static void main(String[] args) {
+        ArrayList<Integer> num = new ArrayList<>();
+        System.out.println(num.size());
+        num.add(10);
+        num.add(11);
+        num.add(12);
+        num.add(3, 20);
+        for (int x : num) {
+            System.out.println(x);
+        }
+        // System.out.println(num);
+        // This can also be used for printing.
+        System.out.println(num.size());
+    }
+}
