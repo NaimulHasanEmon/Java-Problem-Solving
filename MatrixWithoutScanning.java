8a4327ad3cf2ddc11ce402ce8e3d3abd7bd25fa6
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-01-31 20:24:05 +0600

    Update and rename ArrayWithoutScanning.java to MatrixWithoutScanning.java

--- MatrixWithoutScanning.java
+++ MatrixWithoutScanning.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ArrayWithoutScanning {
+public class MatrixWithoutScanning {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter row: ");
