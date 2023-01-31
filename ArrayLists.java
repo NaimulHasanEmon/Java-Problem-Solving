a0f26332eb2683b6ccf26bfb0eb6fa8ad2080dd8
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-02-01 00:52:45 +0600

    Add files via upload

---
+++ ArrayLists.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class ArrayLists {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("How many elements you want to set: ");
+        int n = sc.nextInt();
+        ArrayList<Integer> num = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            num.add(sc.nextInt());
+        }
+        System.out.println(num);
+        sc.close();
+    }
+}
