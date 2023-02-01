a30f39f0e2c1dc9d338b2e619e46ffd44f3bfda9
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-02-01 23:09:42 +0600

    Add files via upload

---
+++ BinaryValueOfEachCharOfString.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BinaryValueOfEachCharOfString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String name = sc.nextLine();
+        System.out.println("ASCII Value of each character is: ");
+        for (int i = 0; i < name.length(); i++) {
+            System.out.println(Integer.toBinaryString(name.codePointAt(i)));
+        }
+        sc.close();
+    }
+}
