59773c47d99bcb49935f852d8bde411424dfe13e
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-02-01 17:49:32 +0600

    Add files via upload

---
+++ SingleLevelInheritance.java
@@ -0,0 +1,24 @@
+class Shape {
+    int base, height;
+
+    public void printArea() {
+        System.out.print("The area is: ");
+    }
+}
+
+class Triangle extends Shape {
+    // Triangle class takes all properties of Shape class
+    public void printColor() {
+        System.out.println(0.5 * base * height);
+    }
+}
+
+public class SingleLevelInheritance {
+    public static void main(String[] args) {
+        Triangle t1 = new Triangle();
+        t1.base = 10;
+        t1.height = 5;
+        t1.printArea();
+        t1.printColor();
+    }
+}
