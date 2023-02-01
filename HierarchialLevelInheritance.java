71e40122e91357dfdb7c08ba1b6085f11aa2a995
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-02-01 18:22:01 +0600

    Add files via upload

---
+++ HierarchialLevelInheritance.java
@@ -0,0 +1,28 @@
+class Shape {
+    int base, height, radius;
+}
+
+class Triangle extends Shape {
+    void printArea() {
+        System.out.print("The area of triangle is: " + (0.5 * base * height));
+    }
+}
+
+class Circle extends Shape {
+    // Triangle class takes all properties of Shape class
+    void printTheArea() {
+        System.out.println("\nThe area of circle is: " + Math.PI * radius * radius);
+    }
+}
+
+public class HierarchialLevelInheritance {
+    public static void main(String[] args) {
+        Circle c1 = new Circle();
+        Triangle t1 = new Triangle();
+        t1.base = 10;
+        t1.height = 5;
+        c1.radius = 15;
+        t1.printArea();
+        c1.printTheArea();
+    }
+}
