141f739c251d665e7fbd02c5bd30a40f0d9ee989
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-02-05 05:17:17 +0600

    Add files via upload

--- DecimalToAll.java
+++ DecimalToAll.java
@@ -3,14 +3,18 @@ import java.util.*;
 public class DecimalToAll {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a decimal number: ");
-        int decimal = sc.nextInt();
-        String bin = Integer.toBinaryString(decimal);
-        String hex = Integer.toHexString(decimal);
-        String oct = Integer.toOctalString(decimal);
-        System.out.println(bin);
-        System.out.println(hex);
-        System.out.println(oct);
+        System.out.print("Enter a binary number: ");
+        String bin = sc.nextLine();
+        Integer dec = Integer.parseInt(bin, 2);
+        System.out.println("Decimal = " + dec);
+        System.out.print("Enter a hexadecimal number: ");
+        String hex = sc.nextLine();
+        Integer dec2 = Integer.parseInt(hex, 16);
+        System.out.println("Decimal = " + dec2);
+        System.out.print("Enter a binary number: ");
+        String oct = sc.nextLine();
+        Integer dec3 = Integer.parseInt(oct, 8);
+        System.out.println("Decimal = " + dec3);
         sc.close();
     }
 }
