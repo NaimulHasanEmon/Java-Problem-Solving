ead6bc2641c56e2cd5060ac8563b6f9c18bd7cf6
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-03-19 14:38:35 +0600

    Update GreatestCommonDivisor.java

--- GreatestCommonDivisor.java
+++ GreatestCommonDivisor.java
@@ -4,18 +4,14 @@ public class GreatestCommonDivisor {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(), m = sc.nextInt();
-        int temp;
-        int p = n, q = m;
+        int temp, p = n, q = m;
         if (n > m) {
             temp = n;
             n = m;
             m = temp;
         }
-        int r, x;
-        int i = 0;
-        do {
-            x = m / n;
-            r = m % n;
+        while (true) {
+            int r = m % n;
             if (r == 0) {
                 System.out.println("Greatest Common Divisor of " + p + " and " + q + " is: " + n);
                 break;
@@ -23,8 +19,7 @@ public class GreatestCommonDivisor {
                 m = n;
                 n = r;
             }
-            i++;
-        } while (r != 0);
+        }
         sc.close();
     }
 }
