d1963c74ad6b1eebd83172e92d275407f667dd79
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-04-03 23:14:27 +0600

    Add files via upload

---
+++ SuperVeriable_1.java
@@ -0,0 +1,19 @@
+class A {
+    int x = 10;
+}
+
+class B extends A {
+    int x = 5;
+
+    void display() {
+        System.out.println(super.x); // super veriable goes to the parent class
+        System.out.println(x);
+    }
+}
+
+public class SuperVeriable_1 {
+    public static void main(String[] args) {
+        B b = new B();
+        b.display();
+    }
+}
