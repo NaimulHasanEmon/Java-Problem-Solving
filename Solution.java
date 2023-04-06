f06080735ecdc6f12284a731d76916ae4ec1bba4
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-04-06 22:11:48 +0600

    Add files via upload
    This problem was one of the beginning tasks of HackerRank.

---
+++ Solution.java
@@ -0,0 +1,32 @@
+import java.math.BigInteger;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        BigInteger[] arr = new BigInteger[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextBigInteger();
+            if (arr[i].compareTo(BigInteger.valueOf(Long.MIN_VALUE)) < 0
+                    || arr[i].compareTo(BigInteger.valueOf(Long.MAX_VALUE)) > 0) {
+                System.out.println(arr[i] + " can't be fitted anywhere.");
+                continue;
+            }
+            System.out.println(arr[i] + " can be fitted in:");
+            if ((arr[i]).compareTo(BigInteger.valueOf(Byte.MIN_VALUE)) >= 0
+                    && (arr[i]).compareTo(BigInteger.valueOf(Byte.MAX_VALUE)) <= 0)
+                System.out.println("* byte");
+            if (arr[i].compareTo(BigInteger.valueOf(Short.MIN_VALUE)) >= 0
+                    && arr[i].compareTo(BigInteger.valueOf(Short.MAX_VALUE)) <= 0)
+                System.out.println("* short");
+            if (arr[i].compareTo(BigInteger.valueOf(Integer.MIN_VALUE)) >= 0
+                    && arr[i].compareTo(BigInteger.valueOf(Integer.MAX_VALUE)) <= 0)
+                System.out.println("* int");
+            if (arr[i].compareTo(BigInteger.valueOf(Long.MIN_VALUE)) >= 0
+                    && arr[i].compareTo(BigInteger.valueOf(Long.MAX_VALUE)) <= 0)
+                System.out.println("* long");
+        }
+        sc.close();
+    }
+}
