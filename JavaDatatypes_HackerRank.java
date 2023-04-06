281d4c328ae2081d458965e8aaae70b889ac2015
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-04-06 22:13:15 +0600

    Update and rename Solution.java to JavaDatatypes_HackerRank.java

--- JavaDatatypes_HackerRank.java
+++ JavaDatatypes_HackerRank.java
@@ -1,7 +1,7 @@
 import java.math.BigInteger;
 import java.util.*;
 
-public class Solution {
+public class JavaDatatypes_HackerRank {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
