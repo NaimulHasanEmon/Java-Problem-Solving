6ea2c8353180d406900471afe7d1e2f123160d32
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-04-12 08:13:54 +0600

    Update Encapsulation.java

--- Encapsulation.java
+++ Encapsulation.java
@@ -1,27 +1,28 @@
-package Bank;
-
+// Encapsulation
 class Account {
-	public String name;
-	protected String email;
-	private String password;
+    public String name;
+    protected String email;
+    private String password;
 
-	// To access private properties, we can use getters & setters
-	public void setPass() {
-		this.password = "abcd";
-	}
+    // To access private properties, we can use getters & setters
+    public void setPass(String a) {
+        this.password = a;
+    }
 
-	public String getPass() {
-		return this.password;
-	}
+    public String getPass() {
+        return this.password;
+    }
 }
 
-public class Bank {
-	public static void main(String[] args) {
-		Account ac = new Account();
-		ac.name = "Emon";
-		ac.email = "emon@gmail.com";
-		ac.setPass();
-		ac.getPass();
-		System.out.println(ac.getPass());
-	}
+public class Main {
+    public static void main(String[] args) {
+        Account ac = new Account();
+        ac.name = "Emon";
+        ac.email = "emon@gmail.com";
+        ac.setPass("abcd");
+        ac.getPass();
+        System.out.println(ac.name);
+        System.out.println(ac.email);
+        System.out.println(ac.getPass());
+    }
 }
