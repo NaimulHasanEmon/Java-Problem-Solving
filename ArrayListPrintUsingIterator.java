a7252269d9820457215b9e90ea5cab34a26a262d
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-04-13 06:09:17 +0600

    Update ArrayListPrintUsingIterator.java

--- ArrayListPrintUsingIterator.java
+++ ArrayListPrintUsingIterator.java
@@ -7,7 +7,7 @@ public class ArrayListPrintUsingIterator {
         num.add(11);
         num.add(12);
         num.add(3, 20);
-        Iterator itr = num.iterator();
+        Iterator<Integer> itr = num.iterator();
         while (itr.hasNext()) {
             System.out.println(itr.next() + " ");
         }
