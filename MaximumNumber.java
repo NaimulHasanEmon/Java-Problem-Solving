b381c5fe4836c1afe5a047cb410af6f89e80577d
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-05-14 11:31:12 +0600

    Update and rename maximumNumber.java to MaximumNumber.java

--- MaximumNumber.java
+++ MaximumNumber.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class maximumNumber {
+public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int a = sc.nextInt();
