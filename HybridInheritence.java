b374f71058f257b2920fb762a9d64ddefe344a9a
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-05-14 11:57:06 +0600

    Update HybridInheritence.java

--- HybridInheritence.java
+++ HybridInheritence.java
@@ -21,7 +21,7 @@ class equilateralTriangle extends Triangle {
     }
 }
 
-public class HybridInheritence {
+public class Main {
     public static void main(String[] args) {
         Circle c = new Circle();
         c.color = "Yello";
