4211fd4009db8ed65ad4723351e330ec9d912e9c
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-05-14 11:31:54 +0600

    Update Abstraction.java

--- Abstraction.java
+++ Abstraction.java
@@ -16,7 +16,7 @@ class Chicken {
     }
 }
 
-public class Abstraction {
+public class Main {
     public static void main(String[] args) {
         Horse horse = new Horse();
         horse.Walk();
