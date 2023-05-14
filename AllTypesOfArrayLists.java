d98129b1ec2dff21dec13615e4ba22d0dac53b95
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-05-14 11:32:12 +0600

    Update AllTypesOfArrayLists.java

--- AllTypesOfArrayLists.java
+++ AllTypesOfArrayLists.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class AllTypesOfArrayLists {
+public class Main {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
 
