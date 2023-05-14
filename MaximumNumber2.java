7b3535f19cbcd758cecbb5a2f4c63ca3071023fd
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-05-14 11:31:27 +0600

    Update and rename maximumNumber2.java to MaximumNumber2.java

--- MaximumNumber2.java
+++ MaximumNumber2.java
@@ -1,4 +1,4 @@
-public class maximumNumber2 {
+public class Main {
     public static void main(String[] args) {
       int a = 4, b = 5, c = 7, d = 9;
       System.out.println("The maximum number is: " + Math.max(a, (Math.max(b, Math.max(c, d)))));
