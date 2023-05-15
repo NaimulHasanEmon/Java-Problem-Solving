223408a23341f3e9fbf805066a33976a404c0655
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-05-15 09:01:13 +0600

    Update ArrayOfString.java

--- ArrayOfString.java
+++ ArrayOfString.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a string: ");
+        System.out.print("Enter 3 strings: ");
         String name[] = new String[3];
         for (int i = 0; i < 3; i++) {
             name[i] = sc.nextLine();
