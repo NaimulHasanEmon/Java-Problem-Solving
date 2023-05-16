25b3f87af8af709fddd3138803295eb7e244ca4c
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-05-16 23:00:33 +0600

    Update Inheritance.java

--- Inheritance.java
+++ Inheritance.java
@@ -11,7 +11,7 @@ class Triangle extends Shape {
     }
 }
 
-public class Inheritance {
+public class Main {
     public static void main(String[] args) {
         Triangle t1 = new Triangle(); // Intantiating a new Triangle object.
         t1.color = "Black";
