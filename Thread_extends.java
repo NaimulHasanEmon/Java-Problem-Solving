9175731b900ec7bd1b4b107b3ab9d0b6c8f18dec
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-05-17 01:29:17 +0600

    Update Thread_extends.java

--- Thread_extends.java
+++ Thread_extends.java
@@ -1,7 +1,8 @@
 class Emon extends Thread {
+    @Override
     public void run() {
         for (int i = 0; i < 5; i++) {
-            System.out.println("Emnon");
+            System.out.println("Emon");
         }
     }
 }
