d116c5d4425fd4c432a40b33ad0a050cb9a570b4
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-05-16 06:54:44 +0600

    Update Message.java

--- Message.java
+++ Message.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Message {
+public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter how many times you want to print: ");
