3830f343ebe39a0b273cc956541a99bc3e8f1778
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-06-01 09:57:26 +0600

    Update Polymorphism.java

--- Polymorphism.java
+++ Polymorphism.java
@@ -16,7 +16,7 @@ class Student {
     }
 }
 
-public class Polymorphism {
+public class Main {
     public static void main(String[] args) {
         Student s1 = new Student();
         s1.name = "Emon";
