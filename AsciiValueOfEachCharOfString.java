e9a02d47635119e5a05865072399bd91897917fb
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-06-15 07:49:01 +0600

    Update AsciiValueOfEachCharOfString.java

--- AsciiValueOfEachCharOfString.java
+++ AsciiValueOfEachCharOfString.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class AsciiValueOfEachCharOfString {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a string: ");
