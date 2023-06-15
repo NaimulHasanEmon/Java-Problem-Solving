7c0deccfed21c6d8bf43fb57fbb475910a3d68de
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-06-15 07:49:15 +0600

    Update BinaryToDecimal.java

--- BinaryToDecimal.java
+++ BinaryToDecimal.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class BinaryToDecimal {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a binary number: ");
