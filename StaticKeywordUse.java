a876169b20322276e78be54dab35d8778d09021b
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-06-15 07:47:33 +0600

    Create StaticKeywordUse.java

---
+++ StaticKeywordUse.java
@@ -0,0 +1,13 @@
+class Emon {
+    public static void print() {
+        System.out.println("Hi! This is Emon");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Emon.print();
+        // We did not created an object of the class Emon
+        // Rather we called the method by it's class name.
+    }
+}
