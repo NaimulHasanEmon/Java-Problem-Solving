e1923b5c7865b038a54b17eb2f3a89157b626ad7
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-07-16 22:25:54 +0600

    Update and rename StringBufferAppend.java to String Buffer Append.java

--- String Buffer Append.java
+++ String Buffer Append.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class StringBufferAppend {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a string: ");
