f97b0cbf34f8ff437d04f1212dd8ac960acdd939
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-07-16 19:41:08 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static boolean check(ArrayList<Integer> arr, int i) {
+        if (i == arr.size() - 1) {
+            return true;
+        }
+        if (arr.get(i) < arr.get(i+1)) {
+            return check(arr, i + 1);
+        } else
+            return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter array elements. If you want to stop input then put \"S\".");
+        ArrayList<Integer> arr = new ArrayList<>();
+        String x;
+        while (true) {
+            x = sc.next();
+            if (x.equals("S")) {
+                break;
+            }
+            arr.add(Integer.parseInt(x));
+        }
+        boolean z = check(arr, 0);
+        if (z)
+            System.out.println("Sorted");
+        else
+            System.out.println("Not sorted");
+    }
+}
