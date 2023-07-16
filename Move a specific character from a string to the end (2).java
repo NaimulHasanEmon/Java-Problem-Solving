6b1ba68a95b2326df81bfb8de370448bbf7b4f77
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-07-16 23:18:58 +0600

    Update and rename Main.java to Move a specific character from a string to the end (2).java

--- Move a specific character from a string to the end (2).java
+++ Move a specific character from a string to the end (2).java
@@ -6,10 +6,12 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a string: ");
         String str = sc.nextLine();
+        System.out.println("Original string = " + str);
         char[] charArray = str.toCharArray();
         System.out.print("Which character you want to move to the end = ");
         char ch = sc.next().charAt(0);
         ArrayList<Character> first = new ArrayList<>();
+        System.out.print("After moving all " + ch + ", the new string is = ");
         for (int i = 0; i < str.length(); i++) {
             if (charArray[i] == ch) {
                 first.add(charArray[i]);
@@ -18,7 +20,7 @@ public class Main {
             }
         }
         StringBuilder sb = new StringBuilder();
-        for(char x : first){
+        for (char x : first) {
             sb.append(x);
         }
         System.out.println(sb.toString());
