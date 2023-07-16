723f5bcd6f12037dbd57ccb759053a2c9121dbb9
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-07-16 18:41:03 +0600

    Update and rename Demo.java to Check if an array of integer is sorted or not.java

--- Check if an array of integer is sorted or not.java
+++ Check if an array of integer is sorted or not.java
@@ -1,9 +1,7 @@
-package Test;
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Demo {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter array elements. If you want to stop input then put \"S\".");
