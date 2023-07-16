262b784ad4f9e650ac60f969b21ec3bbde4d5072
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-07-16 22:18:56 +0600

    Update and rename StringBuilderInJava.java to String Builder.java

--- String Builder.java
+++ String Builder.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class StringBuilderInJava {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a string: ");
