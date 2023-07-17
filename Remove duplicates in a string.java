46a82c7e6bcd3f9373584a375c08ddd4567c27c9
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-07-17 08:52:12 +0600

    Update Remove duplicates in a string.java

--- Remove duplicates in a string.java
+++ Remove duplicates in a string.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Google {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a string: ");
