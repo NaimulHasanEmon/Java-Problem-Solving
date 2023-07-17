4d056d073258cf7f0ca9d2831c8e44b9943f1fd5
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-07-17 09:08:00 +0600

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Demo {
+    public static void delRepeat(String str, int i, StringBuilder sb) {
+        if (i == str.length()) {
+            System.out.println(sb);
+            return;
+        }
+        if (i == str.length() - 1 || str.charAt(i) != str.charAt(i + 1))
+            sb.append(str.charAt(i));
+        else
+            i++;
+        delRepeat(str, i + 1, sb);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        System.out.println("Before remove repetitions = " + str);
+        StringBuilder sb = new StringBuilder();
+        delRepeat(str, 0, sb);
+        sc.close();
+    }
+}
