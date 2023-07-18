5939d9ffe510cbdd88c477646b21213673cce8bf
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-07-18 17:57:50 +0600

    Update and rename Main.java to Permutation using recursion and backtracking.java

--- Permutation using recursion and backtracking.java
+++ Permutation using recursion and backtracking.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void printPermutation(String str, String permutation, int index) {
         if (str.length() == 0) {
@@ -12,7 +14,11 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        String str = "ABC";
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.next();
+        System.out.println("Permutations of " + str + " are: ");
         printPermutation(str, "", 0);
+        sc.close();
     }
 }
