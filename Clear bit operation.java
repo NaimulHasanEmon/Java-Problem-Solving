0a58fb4dc082101c8dad76ea75f4aad37d9e9242
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-07-19 07:25:07 +0600

    Update and rename ClearBitOperation.java to Clear bit operation.java

--- Clear bit operation.java
+++ Clear bit operation.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class ClearBitOperation {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
