2eae575b9a2f2de8dff892d56aef2ae879014f2c
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-07-23 02:05:38 +0600

    Update Greatest Common Divisor.java

--- Greatest Common Divisor.java
+++ Greatest Common Divisor.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class GreatestCommonDivisor {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(), m = sc.nextInt();
