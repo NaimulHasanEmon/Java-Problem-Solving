bec4aea98b3b3233bdad8808273fa2a0fd782e6f
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-07-24 00:07:25 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter two numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("a = " + a + " b = " + b);
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("a = " + a + " b = " + b);
+        sc.close();
+    }
+}
