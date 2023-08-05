769d9aeb4dc816c72b6a499aec6e4cc91de389f5
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-08-05 18:12:51 +0600

    Update and rename LL.java to Binary Search in 2D Arrays.java

--- Binary Search in 2D Arrays.java
+++ Binary Search in 2D Arrays.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class LL {
+class Main {
     public static void main(String[] args) {
         int[][] arr = {{10, 20, 30, 40}, {15, 25, 35, 45}, {28, 29, 37, 49}, {33, 34, 38, 50}};
             Scanner sc = new Scanner(System.in);
