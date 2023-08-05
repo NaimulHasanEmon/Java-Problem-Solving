fb1cb824efc81d2f52461197857165935ed41431
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-08-05 18:13:12 +0600

    Update Binary Search in 2D Arrays.java

--- Binary Search in 2D Arrays.java
+++ Binary Search in 2D Arrays.java
@@ -1,21 +1,21 @@
 import java.util.Scanner;
 
-class Main {
+class LL {
     public static void main(String[] args) {
         int[][] arr = {{10, 20, 30, 40}, {15, 25, 35, 45}, {28, 29, 37, 49}, {33, 34, 38, 50}};
-            Scanner sc = new Scanner(System.in);
-            System.out.print("Enter target number: ");
-            int n = sc.nextInt();
-            int row = 0, col = arr.length - 1;
-            while (row < arr.length && col >= 0) {
-                if (arr[row][col] == n) {
-                    System.out.println("Found at " + row + ", " + col);
-                    break;
-                }
-                if (arr[row][col] > n)
-                    col--;
-                else
-                    row++;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter target number: ");
+        int n = sc.nextInt();
+        int row = 0, col = arr.length - 1;
+        while (row < arr.length && col >= 0) {
+            if (arr[row][col] == n) {
+                System.out.println("Found at " + row + ", " + col);
+                break;
             }
+            if (arr[row][col] > n)
+                col--;
+            else
+                row++;
+        }
     }
 }
