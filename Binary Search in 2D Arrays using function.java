5c1aa8cd1f2a7dfa1e8ead7392dacc192303e876
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-08-05 19:13:36 +0600

    Update Binary Search in 2D Arrays using function.java

--- Binary Search in 2D Arrays using function.java
+++ Binary Search in 2D Arrays using function.java
@@ -5,11 +5,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[][] arr = {{10, 20, 30, 40}, {15, 25, 35, 45}, {28, 29, 37, 44}, {33, 34, 38, 50}};
-        while (true) {
-            System.out.print("Enter your target search: ");
-            int n = sc.nextInt();
-            System.out.println(Arrays.toString(search(arr, n)));
-        }
+        System.out.print("Enter your target search: ");
+        int n = sc.nextInt();
+        System.out.println(Arrays.toString(search(arr, n)));
+        
+        // Time complexity is O(n)
+        
     }
 
     static int[] search(int[][] arr, int n) {
