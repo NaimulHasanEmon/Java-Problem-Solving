99a36aa389e83c485f7c95a15d686d89e6e75dbf
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2023-10-28 12:06:49 +0600

    Update Static.java

--- Static.java
+++ Static.java
@@ -1,12 +1,11 @@
 // Static is for something that is common for all
-package ApnaCollege;
 
 class Student {
     String name;
     static String school; // Here the school name is same for all
 }
 
-public class Static {
+public class Main {
     public static void main(String[] args) {
         Student.school = "BCBHC"; // Input using class name
         Student stu = new Student();
